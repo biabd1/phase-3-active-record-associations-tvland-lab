90645bb1d4d106ee95310e97afe882a1e9b37bfb
Bilal <bilal.ibrahim@student.moringaschool.com>
2022-11-09 20:03:25 +0300

    done

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,13 @@
 class Show < ActiveRecord::Base
-  
+  has_many :characters
+  has_many :actors, through: :characters
+  belongs_to :network
+
+  def actors_list
+    actors_list = []
+    self.actors.each do |actor|
+        actors_list << actor.full_name
+    end
+    actors_list
+end
 end
